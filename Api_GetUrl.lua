0003dc8972a810c62d2c881d56345491a927cda3
OPQBOT <534706350@qq.com>
2020-12-08 15:54:14 +0800

    T

--- Api_GetUrl.lua
+++ Api_GetUrl.lua
@@ -2,6 +2,22 @@ local http = require("http")
 local json = require("json")
 
 function GetUrl(data)
+    local index = GenRandInt(1, 4)
+    if index == 1 then
+        return GetYZFUrl(data)
+    end
+    if index == 2 then
+        return GetTTUrl(data)
+    end
+    if index == 3 then
+        return GetDPUrl(data)
+    end
+    if index == 4 then
+        return GetBJHUrl(data)
+    end
+end
+
+function GetYZFUrl(data)
     response, error_message =
         http.request(
         "POST",
@@ -19,9 +35,87 @@ function GetUrl(data)
             }
         }
     )
-    return json.decode(response.body)
+    jsonData = json.decode(response.body)
+    if jsonData["code"] ~= 0 then
+        return nil
+    end
+
+    return UrlDecode(jsonData["KfPicUrl"])
+end
+--ok
+function GetTTUrl(data)
+    response, error_message =
+        http.request(
+        "POST",
+        "https://mp.toutiao.com/upload_photo/?type=json",
+        {
+            multipart = {},
+            file = {
+                ["photo"] = "./22.jpg",
+                ["body"] = data
+            }
+        }
+    )
+    jsonData = json.decode(response.body)
+    if jsonData.message ~= "success" then
+        return nil
+    end
+    return jsonData.web_url
 end
 
+function GetDPUrl(data)
+    response, error_message =
+        http.request(
+        "POST",
+        "https://kfupload.alibaba.com/mupload",
+        {
+            multipart = {
+                ["scene"] = "productImageRule",
+                ["name"] = "22222.jpg"
+            },
+            file = {
+                ["file"] = "22222.jpg",
+                ["body"] = data
+            }
+        }
+    )
+    jsonData = json.decode(response.body)
+    if jsonData.code ~= "0" then
+        return nil
+    end
+    return jsonData.url
+end
+
+function GetBJHUrl(data)
+    response, error_message =
+        http.request(
+        "POST",
+        "https://baijiahao.baidu.com/builderinner/api/content/file/upload",
+        {
+            multipart = {},
+            file = {
+                ["media"] = "22222.jpg",
+                ["body"] = data
+            }
+        }
+    )
+    jsonData = json.decode(response.body)
+    if jsonData.errno ~= 0 then
+        return nil
+    end
+    return jsonData.ret.https_url
+end
+function UrlDecode(s)
+    s =
+        string.gsub(
+        s,
+        "%%(%x%x)",
+        function(h)
+            return string.char(tonumber(h, 16))
+        end
+    )
+    return s
+end
 function GenRandInt(x, y)
     math.randomseed(tonumber(tostring(os.time()):reverse():sub(1, 6)))
     num = math.random(x, y)
