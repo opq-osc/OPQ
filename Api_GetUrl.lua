6e84756217295715d86c4bdc835be840e25307be
OPQBOT <534706350@qq.com>
2021-05-25 23:12:42 +0800

    NewCdn

--- Api_GetUrl.lua
+++ Api_GetUrl.lua
@@ -1,39 +1,108 @@
 local http = require("http")
+local log = require("log")
 local json = require("json")
 function GetUrl(data)
-    local index = GenRandInt(1, 2)
-    if index == 1 then
-        return GetYZFUrl(data)
-    end 
+    local index = GenRandInt(2, 5)
     if index == 2 then
         return GetBJHUrl(data)
     end
+    if index == 3 then
+        return GetNiuUrl(data)
+    end
+    if index == 4 then
+        return GetVimCNUrl(data)
+    end
+    if index == 5 then
+        return GetOPPOUrl(data)
+    end
 end
-function GetYZFUrl(data)
-    local userid = string.format("kfh53e531627f2ee4c_h552fdfc072182654f163f5f%d", os.time())
-    response, error_message =
+
+function GetNiuUrl(data)
+    local response, error_message = http.request("GET", string.format("http://gocloudcoder.com:8081/upload"))
+
+    jsonData = json.decode(response.body)
+
+    if jsonData.token == nil then
+        return nil
+    end
+    local file_name = string.format("%s%s.jpg", os.time(), GenRandInt(11111, 999999999))
+    local response, error_message =
         http.request(
         "POST",
-        string.format("https://yzf.qq.com/fsna/kf-file/upload_wx_media?_t=%d", os.time()),
+        "http://upload-z2.qiniup.com/",
         {
             multipart = {
-                ["userid"] = userid,
-                ["agentid"] = "",
-                ["media_type"] = "image",
-                ["mid"] = "fsna"
+                ["token"] = jsonData.token,
+                ["key"] = file_name
             },
             file = {
-                ["file"] = "./22.jpg",
+                ["file"] = file_name,
                 ["body"] = data
             }
         }
     )
     jsonData = json.decode(response.body)
-    if jsonData["code"] ~= 0 then
+    if jsonData.key == nil then
         return nil
     end
-
-    return UrlDecode(jsonData["KfPicUrl"])
+    return string.format("https://pic.gocloudcoder.com/%s", jsonData.key)
+end
+function GetVimCNUrl(data)
+    local response, error_message =
+        http.request(
+        "POST",
+        "https://img.vim-cn.com/",
+        {
+            multipart = {},
+            file = {
+                ["image"] = "a.jpg",
+                ["body"] = data
+            }
+        }
+    )
+    return response.body
+end
+function GetOPPOUrl(data)
+    local response, error_message =
+        http.request(
+        "POST",
+        "https://api.open.oppomobile.com/api/utility/upload",
+        {
+            multipart = {
+                ["type"] = "feedback"
+            },
+            file = {
+                ["file"] = "file.jpg",
+                ["body"] = data
+            }
+        }
+    )
+    --log.info("%s", response.body)
+    jsonData = json.decode(response.body)
+    if jsonData.errno ~= 0 then
+        return nil
+    end
+    return jsonData.data.url
+end
+function GetUploadCCUrl(data)
+    local response, error_message =
+        http.request(
+        "POST",
+        "https://mp.yidianzixun.com/upload?action=uploadimage",
+        {
+            multipart = {},
+            file = {
+                ["upfile"] = "file.jpg",
+                ["body"] = data
+            }
+        }
+    )
+    --log.info("%s", response.body)
+    jsonData = json.decode(response.body)
+    if jsonData.errno ~= 0 then
+        return nil
+    end
+    return jsonData.data.url
 end
 function GetBJHUrl(data)
     response, error_message =
