bd7c5a9d3244ac51562c983ff5d25d69ca181e19
fz6m <59400654+fz6m@users.noreply.github.com>
2023-04-02 01:51:17 +0800

    chore: sh not support array in docker image

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,31 +1,31 @@
 #!/bin/sh
 
-run_args=()
+run_args=""
 
 if [[ "$token" == "" ]] ; then
     echo "注意，你没有配置 Token"
 else
-    run_args+=("-token" "$token")
+    run_args="-token $token"
 fi
 
 if [[ "$port" == "" ]] ; then
     echo "将默认运行在 8086 ，注意安全组和防火墙情况，若仅内网使用，请关闭端口防止渗透"
 else
     echo "将运行在 $port 端口"
-    run_args+=("-port" "$port")
+    run_args="$run_args -port $port"
 fi
 
 if [[ "$wsserver" != "" ]] ; then
     echo "ws server: $wsserver"
-    run_args+=("-wsserver" "$wsserver")
+    run_args="$run_args -wsserver $wsserver"
 fi
 
 if [[ "$wthread" != "" ]] ; then
     echo "ws thread: $wthread"
-    run_args+=("-wthread" "$wthread")
+    run_args="$run_args -wthread $wthread"
 fi
 
 echo "开始运行 OPQBot"
 
-echo "运行参数: ${run_args[@]}"
-exec ./opqbot/OPQBot "${run_args[@]}"
+echo "运行参数: $run_args"
+exec ./opqbot/OPQBot $run_args
